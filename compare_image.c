9b3ed18ce311a59d7dc24d0d8a5fa34a440a8fee
Andrew T. Fleming <andrewfleming27@gmail.com>
2023-05-04 17:40:06 -0400

    First test of image combine

--- compare_image.c
+++ compare_image.c
@@ -6,21 +6,23 @@
 
 int main(int argc, char* argv[]) {
   int cflag = 0;
+  int aflag = 0;
   int opt;
-  while ((opt = getopt(argc, argv, "c")) != -1) {
+  while ((opt = getopt(argc, argv, "ca")) != -1) {
     switch (opt) {
-      case 'c': //do we need anything else other than image % comparison?
+      case 'c':
         cflag = 1;
         break;
+      case 'a':
+        aflag = 1;
+        break;
       default:
-        fprintf(stderr, "Usage: %s [-c] file1 file2\n", argv[0]);
+        fprintf(stderr, "Usage for Image Comparison Percent: %s [-c] file1 file2\n", argv[0]);
+        fprintf(stderr, "Usage for Image Combination: %s [-a] file1 file2 outputFile\n", argv[0]);
         return 1;
     }
   }
-  if (argc - optind != 2) {
-    fprintf(stderr, "Usage: %s [-c] file1 file2\n", argv[0]);
-    return 1;
-  }
+
   char command[256];
   if (cflag) {
     sprintf(command, "compare -metric AE -fuzz 10%% %s %s null: 2>&1", argv[optind], argv[optind+1]);
@@ -31,6 +33,9 @@ int main(int argc, char* argv[]) {
     } else {
       printf("Failed to calculate percentage difference.\n");
     }
+  } else if (aflag) {
+    sprintf(command, "composite -gravity center %s %s %s", argv[optind], argv[optind+1], argv[optind+2]);
+    int result = system(command);
   }
   return 0;
 }
