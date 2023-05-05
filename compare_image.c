c68b0b8782ff9d1a21b6f4191ee96a4f665fb305
TromboneDavies <thomasdavies2020@gmail.com>
2023-05-04 22:10:54 -0400

    refine andrew's compare file

--- compare_image.c
+++ compare_image.c
@@ -7,8 +7,9 @@
 int main(int argc, char* argv[]) {
   int cflag = 0;
   int aflag = 0;
+  int bflag = 0;
   int opt;
-  while ((opt = getopt(argc, argv, "ca")) != -1) {
+  while ((opt = getopt(argc, argv, "cba")) != -1) {
     switch (opt) {
       case 'c':
         cflag = 1;
@@ -16,8 +17,11 @@ int main(int argc, char* argv[]) {
       case 'a':
         aflag = 1;
         break;
+      case 'b':
+        bflag = 1;
+        break;
       default:
-        fprintf(stderr, "Usage for Image Comparison Percent: %s [-c] file1 file2\n", argv[0]);
+        fprintf(stderr, "Usage for Image Comparison: %s [-c] file1 file2 file3\n", argv[0]);
         fprintf(stderr, "Usage for Image Combination: %s [-a] file1 file2 outputFile\n", argv[0]);
         return 1;
     }
@@ -25,17 +29,16 @@ int main(int argc, char* argv[]) {
 
   char command[256];
   if (cflag) {
-    sprintf(command, "compare -metric AE -fuzz 10%% %s %s null: 2>&1", argv[optind], argv[optind+1]);
-    int result = system(command);
-    float diff;
-    if (sscanf(command, "%f", &diff) == 1) {
-      printf("Percentage Difference: %d %%\n", (int)diff);
-    } else {
-      printf("Failed to calculate percentage difference.\n");
-    }
+    sprintf(command, "compare %s %s %s: 2>&1", argv[optind], argv[optind+1], argv[optind+1]);
+    printf("%s", command);
+    system(command);
   } else if (aflag) {
     sprintf(command, "composite -gravity center %s %s -blend 50x50 %s", argv[optind], argv[optind+1], argv[optind+2]);
     int result = system(command);
+  } else if (bflag) {
+    sprintf(command, "composite -gravity center %s %s %s", argv[optind], argv[optind+1], argv[optind+2]);
+    int result = system(command);
+
   }
   return 0;
 }
