d7d487b7c17e2767f3d3e742a9c71657bbc303ea
TromboneDavies <thomasdavies2020@gmail.com>
2023-05-05 20:09:00 -0400

    Update test.sh to not use external txt file

--- test.sh
+++ test.sh
@@ -4,10 +4,9 @@ echo "==========Testing Deterministic Options for Correctness=========="
 echo ""
 ./serial -d input.png output.png > /dev/null
 ./par -d input.png output2.png > /dev/null
-compare -channel all -metric mae output.png output2.png diff.png > result.txt 2>&1
-grep '0 (0)' result.txt > /dev/null
+result=$(compare -channel all -metric mae output.png output2.png diff.png 2>&1)
 
-if [ $? == 0 ]; then
+if [ "$result" = "0 (0)" ]; then
     echo "desaturation test... passed"
 else
     echo "desaturation test... failed"
@@ -15,10 +14,9 @@ fi
 
 ./serial -g 10 input.png output.png > /dev/null
 ./par -g 10 input.png output2.png > /dev/null
-compare -channel all -metric mae output.png output2.png diff.png > result.txt 2>&1
-grep '0 (0)' result.txt > /dev/null
+result=$(compare -channel all -metric mae output.png output2.png diff.png 2>&1)
 
-if [ $? == 0 ]; then
+if [ "$result" = "0 (0)" ]; then
     echo "Gaussian Blur test... passed"
 else
     echo "Gaussian Blur test... failed"
@@ -26,10 +24,9 @@ fi
 
 ./serial -r input.png output.png > /dev/null
 ./par -r input.png output2.png > /dev/null
-compare -channel all -metric mae output.png output2.png diff.png > result.txt 2>&1
-grep '0 (0)' result.txt > /dev/null
+result=$(compare -channel all -metric mae output.png output2.png diff.png 2>&1)
 
-if [ $? == 0 ]; then
+if [ "$result" = "0 (0)" ]; then
     echo "Rotate test... passed"
 else
     echo "Rotate test... failed"
